d87aeb502192f5ece1213f526d71f143903f1242
Ülkü <93094752+Ulku-dur@users.noreply.github.com>
2024-12-02 21:04:09 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import Loading from "./components/Loading";
+import Tours from "./components/Tours";
+import { useEffect, useState } from "react";
+
+const url = "https://www.course-api.com/react-tours-project";
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours)
+  }
+  const fetchTours = async () => {
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return <main>
+    <Tours tours={tours} removeTour={removeTour}/>
+  </main>;
+};
+
+export default App;
